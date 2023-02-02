12c202621e277aa87d5360320be888c0d5e11741
Ashleyjv <54588325+Ashleyjv@users.noreply.github.com>
2023-02-02 07:39:24 +0530

    final Push

---
+++ SocialMediaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SocialMediaViewController.swift
+//  MyStory2
+//
+//  Created by Ashley J Varghese on 2/2/23.
+//
+
+import UIKit
+
+class SocialMediaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
